d311bdb109c1faf76b526cbf5f21a21e353ed5d0
Jan Onderek <jan.onderek@bileto.com>
2018-07-27 06:03:02 +0200

    (CHECKER) Improve Grid slightly and adjust example

--- src/Checker/FieldStatus.hpp
+++ src/Checker/FieldStatus.hpp
@@ -4,6 +4,36 @@
 #include <utility>
 #include <vector>
 
+/*namespace Logicker::Util {
+  template<class ElemType>
+  using Container = std::vector<ElemType>;
+  template<class ElemType>
+  using ElemIt = typename Container<ElemType>::iterator;
+  template<class ElemType>
+  class ElemRange {
+    public:
+      ElemRange(const ElemIt<ElemType>& begin, const ElemIt<ElemType>& end);
+      ElemIt<ElemType> begin() const;
+      ElemIt<ElemType> end() const;
+    private:
+      const ElemIt<ElemType> begin_;
+      const ElemIt<ElemType> end_;
+  };
+
+  template<class ElemType>
+  ElemRange<ElemType>::ElemRange(const ElemIt<ElemType>& begin, const ElemIt<ElemType>& end) : begin_{begin}, end_{end} {}
+
+  template<class ElemType>
+  ElemIt<ElemType> ElemRange<ElemType>::begin() const {
+    return begin_;
+  }
+
+  template<class ElemType>
+  ElemIt<ElemType> ElemRange<ElemType>::end() const {
+    return end_;
+  }
+}*/
+
 //it is implicitly expected values form a linear structure, while that's
 //not the case (the structure can be nonexistent (set of values when
 //order doesn't matter) or nonlinear (e.g. square for e.g. loops))
@@ -60,25 +90,25 @@ namespace Logicker::Checker {
 //some preprocessing should be present in this (most importantly excluding empties,
 //i.e. not putting empties into values array)
 namespace Logicker::Checker {
-  template<class Value, class FieldCoords>
+  template<class Value, class Topology>
   class GridCondition {
     public:
-      GridCondition(const Condition<Value>& cond, const std::vector<FieldCoords>& fields);
-      bool is_satisfied_by(const SolutionGrid<Value, FieldCoords>& grid) const;
+      GridCondition(const Condition<Value>& cond, const std::vector<typename Topology::Coords>& fields);
+      bool is_satisfied_by(const SolutionGrid<Value, typename Topology::Coords>& grid) const;
     private:
       const Condition<Value>& cond_;
-      const std::vector<FieldCoords>& fields_;
+      const std::vector<typename Topology::Coords> fields_;
   };
 
-  template<class Value, class FieldCoords>
-  GridCondition<Value, FieldCoords>::GridCondition(const Condition<Value>& cond, const std::vector<FieldCoords>& fields) : cond_{cond}, fields_{fields} {}
+  template<class Value, class Topology>
+  GridCondition<Value, Topology>::GridCondition(const Condition<Value>& cond, const std::vector<typename Topology::Coords>& fields) : cond_{cond}, fields_{fields} {}
 
-  template<class Value, class FieldCoords>
+  template<class Value, class Topology>
   bool
-  GridCondition<Value, FieldCoords>::is_satisfied_by(const SolutionGrid<Value, FieldCoords>& grid) const {
+  GridCondition<Value, Topology>::is_satisfied_by(const SolutionGrid<Value, typename Topology::Coords>& grid) const {
     std::vector<Value> vals;
     //std::transform(fields_.begin(), fields_.end(), std::back_inserter(vals),
-    //    [grid](const FieldCoords& coord) { return grid.get_value(coord); });
+    //    [grid](const Topology::Coords& coord) { return grid.get_value(coord); });
     for (auto coord : fields_) {
       vals.push_back(grid.get_value(coord));
     }
@@ -87,20 +117,9 @@ namespace Logicker::Checker {
 }
 
 namespace Logicker::Topology {
-  template<class ElemType>
-  using Container = std::vector<ElemType>;
-  template<class ElemType>
-  using ElemIt = typename Container<ElemType>::iterator;
-  template<class ElemType>
-  using ElemRange = std::pair<ElemIt<ElemType>, ElemIt<ElemType>>;
-
   using CoordsMetaGroup = std::string;
   using Index = int;
   using Direction = bool;//true for normal direction, false for reverse
-
-  using CoordsGroup = std::tuple<CoordsMetaGroup, Index, Direction>;
-  using CoordsGroupIt = Container<CoordsGroup>::iterator;
-  using CoordsGroupRange = std::pair<CoordsGroupIt, CoordsGroupIt>;
 
   static CoordsMetaGroup CMG_Rows = "Rows";
   static CoordsMetaGroup CMG_Cols = "Cols";
@@ -108,25 +127,76 @@ namespace Logicker::Topology {
 
   class Rectangle {
     public:
+      typedef std::pair<int, int> Size ;
       typedef std::pair<int, int> Coords ;
-      typedef ElemRange<Coords> CoordsRange ;
+      typedef std::vector<Coords> CoordsRange ;
+      typedef std::tuple<CoordsMetaGroup, Index, Direction> CoordsGroup ;
+      typedef std::vector<CoordsGroup> CoordsGroupRange ;
 
       //tohle vsechno udelat static; topologie nebudou instanciovatelne, neni proc
-      Rectangle(int rows, int cols);
-      CoordsRange get_all_coords();
+      static CoordsRange get_all_coords(Size size);
 
-      CoordsGroupRange get_all_coords_groups();
-      CoordsRange get_coords_in_group(CoordsGroup group);
+      static CoordsGroupRange get_all_coords_groups(Size size);
+      static CoordsRange get_coords_in_group(Size size, CoordsGroup group);
 
-      CoordsGroupRange get_coords_groups(CoordsMetaGroup, Direction);
-    
+      static CoordsGroupRange get_coords_groups(Size size, CoordsMetaGroup cmg, Direction direction);
   };
+
+  Rectangle::CoordsGroupRange
+  Rectangle::get_all_coords_groups(Size size) {
+    std::vector<CoordsGroup> result_vec;
+    for (int i = 0; i < size.first; ++i) {
+      result_vec.push_back( {CMG_Rows, i, true } );
+    }
+    for (int i = 0; i < size.second; ++i) {
+      result_vec.push_back( {CMG_Cols, i, true } );
+    }
+    return result_vec;
+  }
+
+  Rectangle::CoordsRange
+  Rectangle::get_coords_in_group(Size size, CoordsGroup group) {
+    std::vector<Coords> result_vec;
+    CoordsMetaGroup cmg{ std::get<0>(group) };
+    Index index{ std::get<1>(group) };
+    Direction dir{ std::get<2>(group) };
+    if (cmg == CMG_Rows) {
+      for (int col = 0; col < size.second; ++col) {
+        result_vec.push_back({index, col});
+      }
+    } else if (cmg == CMG_Cols) {
+      for (int row = 0; row < size.first; ++row) {
+        result_vec.push_back({row, index});
+      }
+    }
+    return result_vec;
+  }
 }
 
 namespace Logicker {
   template<class FieldType, class Topology>
   class Grid {
     public:
-      Grid(int num_rows, int num_cols);
+      Grid(typename Topology::Size size);
+
+      typename Topology::CoordsGroupRange get_all_coords_groups();
+      typename Topology::CoordsRange get_coords_in_group(
+          typename Topology::CoordsGroup group);
+    private:
+      typename Topology::Size size_;
   };
+
+  template<class FieldType, class Topology>
+  Grid<FieldType, Topology>::Grid(typename Topology::Size size) : size_{size} {}
+
+  template<class FieldType, class Topology>
+  typename Topology::CoordsGroupRange Grid<FieldType, Topology>::get_all_coords_groups() {
+    return Topology::get_all_coords_groups(size_);
+  }
+
+  template<class FieldType, class Topology>
+  typename Topology::CoordsRange Grid<FieldType, Topology>::get_coords_in_group(
+      typename Topology::CoordsGroup group) {
+    return Topology::get_coords_in_group(size_, group);
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,15 @@ int main() {
   using FieldType = int;
   using Top = Logicker::Topology::Rectangle;
   using Coords = Top::Coords;
-  //Logicker::Grid<FieldType, Top> grid{ 2, 2 };
+  Top::Size grid_size{ 2, 2 };
+  Logicker::Grid<FieldType, Top> grid{ grid_size };
 
-  std::vector<Logicker::Checker::GridCondition<int, Coords>> grid_conds;
-
-  std::vector<Coords> coords1 {{0, 0}, {0, 1}};//row1
-  grid_conds.push_back(
-      Logicker::Checker::GridCondition<int, Coords>(Logicker::neq, coords1));
-  std::vector<Coords> coords2 {{1, 0}, {1, 1}};//row2
-  grid_conds.push_back(
-      Logicker::Checker::GridCondition<int, Coords>(Logicker::neq, coords2));
-  std::vector<Coords> coords3 {{0, 0}, {1, 0}};//col1
-  grid_conds.push_back(
-      Logicker::Checker::GridCondition<int, Coords>(Logicker::neq, coords3));
-  std::vector<Coords> coords4 {{0, 1}, {1, 1}};//col2
-  grid_conds.push_back(
-      Logicker::Checker::GridCondition<int, Coords>(Logicker::neq, coords4));
+  std::vector<Logicker::Checker::GridCondition<int, Top>> grid_conds;
+  for (auto coords_group : grid.get_all_coords_groups()) {
+    grid_conds.push_back(
+        Logicker::Checker::GridCondition<int, Top>(Logicker::neq, 
+          grid.get_coords_in_group(coords_group)));
+  }
 
   std::map<Coords, int> grid_vals;
   Coords c0{0, 0};
