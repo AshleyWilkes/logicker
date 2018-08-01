76d277a06ffbe088866a2e5bfa33fa841c2d4b74
Jan Onderek <jan.onderek@bileto.com>
2018-08-02 00:48:36 +0200

    (CHECKER) Init puzzle_instance_factory from a json

--- src/core/input_node.hpp
+++ src/core/input_node.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <json/json.h>
 #include <map>
 #include <memory>
 #include <string>
@@ -15,12 +16,15 @@ namespace logicker::core {
       virtual ~input_node_base() = default;
 
       virtual std::string name() const = 0;
+
+      static std::shared_ptr<input_node_base> json_to_node( std::string name, const Json::Value& value );
   };
 
   class composite_input_node : public input_node_base {
     public:
       composite_input_node(std::string name, std::map<std::string, std::shared_ptr<input_node_base>> parts)
           : name_{ name }, parts_{ std::move(parts) } {}
+      composite_input_node(std::string name, const Json::Value& value);
       composite_input_node(const composite_input_node& rhs) : name_{ rhs.name_ }, parts_{ rhs.parts_ } {}
       composite_input_node& operator=(composite_input_node&& rhs) { name_ = std::move( rhs.name_ ); parts_ = std::move( rhs.parts_ ); }
       std::string name() const override { return name_; }
@@ -48,6 +52,32 @@ namespace logicker::core {
       std::string name_;
       int value_;
   };
+
+  std::shared_ptr<input_node_base>
+  input_node_base::json_to_node( std::string name, const Json::Value& value ) {
+    if ( value.isObject() || value.isArray() ) {
+      return std::make_shared<composite_input_node>(composite_input_node{ name, value });
+    } else if ( value.isInt() ) {
+      return std::make_shared<int_input_node>(int_input_node{ name, value.asInt() });
+    } else {
+      throw "input_node_base::json_to_node() error: unsupported value type";
+    }
+  }
+
+  composite_input_node::composite_input_node(std::string name, const Json::Value& value) : name_{ name } {
+    if ( value.isObject() ) {
+      for ( auto member_name : value.getMemberNames() ) {
+        parts_.insert({ member_name, input_node_base::json_to_node( member_name, value[member_name] ) });
+      }
+    } else if ( value.isArray() ) {
+      for ( int i = 0; i < value.size(); ++i ) {
+        std::string name{ "#" + i };
+        parts_.insert({ name, input_node_base::json_to_node( name, value[i] ) });
+      }
+    } else {
+      throw "composite_input_node::constr() error: unsupported value type";
+    }
+  }
 
   template<class target_input_node>
   const target_input_node&
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,22 @@
 #include "core/puzzle_instance_factory.hpp"
 #include "core/checker.hpp"
 #include "core/solver.hpp"
+#include <fstream>
 #include <iostream>
+#include <json/json.h>
 #include <map>
 #include <memory>
+#include <streambuf>
+#include <string>
 #include <vector>
 
 int main() {
-  std::cout << "This is Logicker!\n";
-  //tak to by neslo, potrebuju classu solver (a napred checker)
-  /*try {
-    using puzzle_type = logicker::puzzle::latin_square<2>;
-    using field_type = puzzle_type::field_type;
-    using topology = puzzle_type::topology;
-    puzzle_type puzzle;
-
-    puzzle.get_field({0, 0}).set_value(1);
-    while ( auto deduction = puzzle.try_to_find_deduction()) {
-      puzzle.apply_deduction(deduction);
-    }
-    std::cout << puzzle.get_grid();
-  } catch (char const* e) {
-    std::cout << e;
-  }*/
-
   try {
+  std::cout << "This is Logicker!\n";
+  std::ifstream is{ "latin_square2x2.lgc" };
+  std::string input_str{ std::istreambuf_iterator<char>( is ),
+    std::istreambuf_iterator<char>() };
+
   using logicker::puzzle::latin_square;
   using logicker::core::puzzle_instance;
   using factory_t = logicker::core::puzzle_instance_factory<latin_square>;
@@ -38,35 +30,23 @@ int main() {
   using int_input = logicker::core::int_input_node;
   using composite_input = logicker::core::composite_input_node;
 
-  std::map<std::string, std::shared_ptr<base_input>> map;
-  std::shared_ptr<base_input> size_input = std::make_shared<int_input>(int_input{ "Size", 2 });
   std::vector<int> vals {
     1,2,
     2,1};
-  /*std::shared_ptr<base_input> size_input = std::make_shared<int_input>(int_input{ "Size", 4 });
-  std::vector<int> vals {
+  /*std::vector<int> vals {
     1,2,3,4,
     3,4,1,2,
     2,3,4,1,
     4,1,2,3};*/
 
-  map.insert({ "Size", std::move(size_input) });
+  Json::Value input_json_value;
+  Json::Reader reader;
+  if (! reader.parse(input_str, input_json_value, false)) {
+    throw "parsing json error: probably malformed syntax\n";
+  }
 
-  std::map<std::string, std::shared_ptr<base_input>> givens_map;
-  std::map<std::string, std::shared_ptr<base_input>> given1_map;
-  std::shared_ptr<int_input> row_input = std::make_shared<int_input>(int_input{ "Row", 0 });
-  given1_map.insert({ "Row", row_input });
-  std::shared_ptr<int_input> col_input = std::make_shared<int_input>(int_input{ "Col", 0 });
-  given1_map.insert({ "Col", col_input });
-  std::shared_ptr<int_input> val_input = std::make_shared<int_input>(int_input{ "Value", 1 });
-  given1_map.insert({ "Value", val_input });
-  std::shared_ptr<base_input> given1_input = std::make_shared<composite_input>(composite_input{ "[0, 0]", given1_map });
+  composite_input input{ "Input", input_json_value };
 
-  givens_map.insert({ "[0, 0]", given1_input });
-  std::shared_ptr<base_input> givens_input = std::make_shared<composite_input>(composite_input{ "Givens", givens_map });
-  map.insert({ "Givens", std::move(givens_input) });
-
-  composite_input input{ "Input", map };
   puzzle_t puzzle_inst = factory_t::create( input );
   checker_t checker{ std::move(puzzle_inst) };
 
